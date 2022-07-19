ba030f71c71a27a587c0de2b03e020531e9cfdaf
Martin Zaninovich <martinzaninovich5@gmail.com>
2022-07-19 09:21:45 -0300

    Entrega de Ejercicio Tema 2

---
+++ Ejercicio_Tema_2/index.js
@@ -0,0 +1,14 @@
+const array = [
+    nombre = "Martín Zaninovich",
+    edad = 36,
+    esProgramador = true,
+    fechaNacimiento = new Date(1986, 4, 28),
+    libroFavorito = {
+        título: "The Great Gatsby",
+        autor: "F. Scott Fitzgerald",
+        fecha: "10 de abril de 1925",
+        url: "https://www.amazon.com/-/es/F-Scott-Fitzgerald-ebook/dp/B000FC0PDA/ref=sr_1_2?crid=MZPK0RH0S8GG&keywords=the+great+gatsby&qid=1657550127&s=books&sprefix=the+g%2Cstripbooks-intl-ship%2C234&sr=1-2"
+        }
+]
+
+console.log(array);
