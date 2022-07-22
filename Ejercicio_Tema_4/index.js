8f394e6c23815a6b93c1da1703063c2425052104
Martin Zaninovich <martinzaninovich5@gmail.com>
2022-07-22 06:41:13 -0300

    entrega_ejercicio_tema_4

---
+++ Ejercicio_Tema_4/index.js
@@ -0,0 +1,18 @@
+let nombre = "Martín";
+let apellido = "Zaninovich";
+
+let estudiante = `${nombre} ${apellido}`;
+
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+
+let longitud = estudiante.length;
+
+let primeraLetraNombre = nombre.charAt(0);
+let ultimaLetraApellido = apellido.charAt(9);
+
+let eliminarEspacios = estudiante.trim();
+
+let estaElNombre = estudiante.includes(nombre);
+
+
