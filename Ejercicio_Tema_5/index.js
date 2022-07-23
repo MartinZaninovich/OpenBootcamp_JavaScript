e142ea4f6f23bdbb381f4762c7aaec49222182e8
Martin Zaninovich <martinzaninovich5@gmail.com>
2022-07-23 15:54:30 -0300

    Correción de Ejercicio Tema 5

--- Ejercicio_Tema_5/index.js
+++ Ejercicio_Tema_5/index.js
@@ -1,6 +1,6 @@
 let altInt = 186;
 let altFloat = 1.86;
 let pesoFloat = 73.5;
-let altRedondeadaArriba = Math.round(altFloat);
+let altRedondeadaArriba = Math.ceil(altFloat);
 let pesoRedondeadoAbajo = Math.floor(pesoFloat)
-let max_valor = Number.MAX_VALUE + 1;
+let max_valor = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
