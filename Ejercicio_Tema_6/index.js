f6fcf20adab70e322943f50911eebfa256a5c132
Martin Zaninovich <martinzaninovich5@gmail.com>
2022-07-24 07:50:29 -0300

    Corrección Ejercicio Tema 6

--- Ejercicio_Tema_6/index.js
+++ Ejercicio_Tema_6/index.js
@@ -3,11 +3,11 @@ listaCompras.push("aceite de girasol");  // Añadiendo "aceite de girasol"
 listaCompras.pop("aceite de girasol");   // Eliminando "aceite de girasol"
 
 let peliFav = [
-    {titulo: "Terminator 2", director: "James Cameron", fecha: 1991},
-    {titulo: "Tiburón", director: "Steven Spielberg", fecha: 1975},
-    {titulo: "Manhattan", director: "Woody Allen", fecha: 1979},
+    {titulo: "Terminator 2", director: "James Cameron", fecha: new Date(1991, 7, 8)},
+    {titulo: "Tiburón", director: "Steven Spielberg", fecha: new Date(1975, 6, 31)},
+    {titulo: "Manhattan", director: "Woody Allen", fecha: new Date(1980, 4, 22)},
 ];
-let peliPost90 = peliFav.filter(peli => peli.fecha > 1990);
+let peliPost90 = peliFav.filter(peli => peli.fecha > new Date(1900, 0, 1));
 let directores = peliFav.map(peli => peli.director);
 let titulos = peliFav.map(peli => peli.titulo);
 let listaDirTit = directores.concat(titulos);
