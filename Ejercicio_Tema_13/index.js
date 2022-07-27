bdaff34eb861dae89111babe817ea020b9e70c47
Martin Zaninovich <martinzaninovich5@gmail.com>
2022-07-27 06:59:30 -0300

    Entrega Ejercicio Tema 13

---
+++ Ejercicio_Tema_13/index.js
@@ -0,0 +1,18 @@
+let nombre = "Martín"
+let apellido = "Zaninovich"
+
+let estudiante = nombre.concat(" ").concat(apellido)
+
+let estudianteMayus = estudiante.toUpperCase()
+let estudianteMinus = estudiante.toLowerCase()
+
+let longitud = estudiante.length
+
+let primeraLetraNombre = nombre.charAt(0)
+let ultimaLetraApellido = apellido.charAt(apellido.length - 1)
+
+let eliminarEspacios = estudiante.replace(" ", " ")
+
+let estaElNombre = estudiante.includes(nombre)
+
+
